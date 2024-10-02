e61292ab7f006f355381c818e68379df54fe7513
deloygintaras <turcinasgintaras@gmail.com>
2024-10-02 19:57:14 +0300

    done

--- main.py
+++ main.py
@@ -119,10 +119,14 @@
 #     print("Režis - klaidingas")
 # else:
 #     for i in range(pradzia, pabaiga + (1 if zingsnis > 0 else -1), zingsnis):
-#         if (pasirinkimas == 'lyginius' and i % 2 == 0) or (pasirinkimas == 'nelyginius' and i % 2 != 0):
+#         if (pasirinkimas == 'lyginis' and i % 2 == 0) or (pasirinkimas == 'nelyginis' and i % 2 != 0):
 #             print(i)
-
-
+#
+# isEven = skaicius == "lyginius"
+# for i in range(pradzia, pabaiga +1, zingsnis):
+#     if isEven and i % 2 == 0:
+#         print(i)
+#         elif not isEven
 
 # 12.Su for pagalba, pamėginkite išvesti tokią eglutę:
 # *
@@ -141,6 +145,7 @@
 # būti pakartojama kiekviena raidė. Su ciklo pagalba išveskite kiekvieną
 # raidę iš žodžio atskiroje eilutėje, taip pat, tą raidę eilutėje kartokite tiek
 # kartų kiek pasirinko vartotojas (16 pvz).
+
 # zodis = str(input("Iveskite norima zodi\n"))
 # pakart = int(input("kiek kartu turi buti pakartojama kiekviena raide?\n"))
 # for i in zodis:
@@ -157,11 +162,12 @@
 #     for _ in range(b):
 #         result += a
 #     return result
-# print(f"{a} * {b} = {multiply(a, b)}")
+
 
 
 # 15.Raskite visų skaičių nuo 1 iki 100 sumą.
-suma = 0
+
+# suma = 0
 # for i in range(100):
 #     suma += i
 # print(f"gauta suma = {suma}")
@@ -188,9 +194,11 @@ suma = 0
 # Visi skaičiai mažesni už 10 ir kurie dalinasi iš 3 arba 5 yra: 3, 5, 6, 9.
 # Šių skaičių suma yra 23.
 # Turite gauti 233168 atsakymą.
-
-
-
+# suma = 0
+# for i in range(0, 1000):
+#     if i % 3 == 0 or i % 5 == 0:
+#         suma += i
+#         print(f"Gauta suma is skaiciu < 1000 ir kurie dalinasi is 3 arba 5 = {suma}")
 
 
 
@@ -203,7 +211,16 @@ suma = 0
 # of three print “Fizz” instead of the number and for the multiples of five
 # print “Buzz”. For numbers which are multiples of both three and five print
 # “FizzBuzz”."
-
+#
+# for i in range(1, 100):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print(f"{i} = FizzBuzz")
+#     elif i % 3 == 0:
+#         print(f"{i} = Fizz")
+#     elif i % 5 == 0:
+#         print(f"{i} = Buzz")
+#     else:
+#         print(i)
 
 # 20.(sunkesnė) Parašyti for ciklą, kuris išvestų norimą kiekį fibonačiaus skaičių
 # į ekraną. Fibonačiaus sekoje kiekvienas skaičius yra lygus prieš jį ėjusių
@@ -213,5 +230,13 @@ suma = 0
 # 2. Trečiasis bus šių pirmų dviejų skaičių suma.
 # 2. Pirmus du skaičius išveskite ne cikle, o prieš jį ir ciklą pradėkite vykdyti nuo 2, o ne nuo 0.
 # 3. Kiekvieno ciklo metu turite perskaičiuot trečiąjį skaičių (pirmų dviejų skaičių sudėtis),
-# tuomet pirmasis skaičius yra lygus antram, o antrasis lygus trečiam, išvesti į ekraną trečią
-# skaičių.
+# tuomet pirmasis skaičius yra lygus antram, o antrasis lygus trečiam, išvesti į ekraną trečią skaičių.
+
+# n = 25
+# sk1 = 5
+# sk2 = 7
+# print(f"Pirmasis kintamasis = {sk1}")
+# print(f"Antras kintamasis = {sk2}")
+# for i in range(2, n):
+#     sk3 = sk1 + sk2
+#     print(f"Trecias kintamasis = {sk3}")
